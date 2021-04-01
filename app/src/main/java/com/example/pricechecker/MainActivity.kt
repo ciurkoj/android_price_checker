924a3e00ac4653ca196791352af9b25e2ef4370c
ciurkoj <ciurkoj@coventry.ac.uk>
2021-04-01 01:30:57 +0100

    A barcode scanner from Google Vision API
    The barcode scanner has been added to the app
    
    TODO: Build the manual search activity and implement SerpApi to the project.

--- app/src/main/java/com/example/pricechecker/MainActivity.kt
+++ app/src/main/java/com/example/pricechecker/MainActivity.kt
@@ -3,34 +3,44 @@ package com.example.pricechecker
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.*
-import android.widget.Button
+import android.util.SparseArray
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.pricechecker.barcode_reader.BarcodeReaderActivity
+import com.example.pricechecker.barcode_reader.BarcodeReaderFragment
+import com.example.pricechecker.fragments.BarcodeFragment
 import com.example.pricechecker.fragments.ManualFragment
 import com.example.pricechecker.fragments.RecentFragment
 import com.example.pricechecker.fragments.ScanFragment
 import com.example.pricechecker.fragments.adapters.ViewPagerAdapter
 import com.example.pricechecker.ui.login.LoginActivity
+import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BarcodeReaderFragment.BarcodeReaderListener {
 
+    private val BARCODE_READER_ACTIVITY_REQUEST = 1208
+    private val mTvResult: TextView? = null
+    private val mTvResultHeader: TextView? = null
+//    private var btn_fragment: Button = findViewById<Button>(R.id.btn_fragment)
 
     //    private val currentUser: Any
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -44,17 +54,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 //        val currentUser = Firebase.auth.currentUser
-
+        findViewById<TextView>(R.id.tv_result)
+        findViewById<TextView>(R.id.tv_result_head)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
                 R.id.nav_home,
 //                R.id.nav_gallery,
 //                R.id.nav_slideshow,
-                R.id.settings_activity), drawerLayout)
+                R.id.settings_activity
+            ), drawerLayout
+        )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
@@ -78,14 +92,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
     private fun setUpTabs() {
         val adapter = ViewPagerAdapter(supportFragmentManager)
 
         adapter.addFragment(RecentFragment(), "Recent")
         adapter.addFragment(ScanFragment(), "Scan")
         adapter.addFragment(ManualFragment(), "Manual")
-
+        adapter.addFragment(BarcodeFragment(), "Barcode")
 
         viewPager.adapter = adapter
         tabs.setupWithViewPager(viewPager)
@@ -95,9 +108,9 @@ class MainActivity : AppCompatActivity() {
         R.id.action_settings -> {
             // User chose the "Settings" item, show the app settings UI...
             Toast.makeText(
-                    applicationContext,
-                    "welcome displayName",
-                    Toast.LENGTH_LONG
+                applicationContext,
+                "welcome displayName",
+                Toast.LENGTH_LONG
             ).show()
             startActivity(Intent(this, SettingsActivity::class.java))
             true
@@ -129,6 +142,7 @@ class MainActivity : AppCompatActivity() {
         finish()
         // [END auth_sign_out]
     }
+
     private fun updateNavHeader() {
         val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
         val headerView = navigationView.getHeaderView(0)
@@ -147,6 +161,26 @@ class MainActivity : AppCompatActivity() {
         // first we need to import the library
 //        Glide.with(this).load(currentUser.getPhotoUrl()).into(navUserPhot)
 
+    }
+
+    override fun onScanned(barcode: Barcode?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onScannedMultiple(barcodes: MutableList<Barcode>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBitmapScanned(sparseArray: SparseArray<Barcode>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onScanError(errorMessage: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCameraPermissionDenied() {
+        TODO("Not yet implemented")
     }
 
 
---
+++ app/src/main/java/com/example/pricechecker/fragments/ScanFragment.java
@@ -0,0 +1,115 @@
+package com.example.pricechecker.fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.pricechecker.R;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.example.pricechecker.barcode_reader.BarcodeReaderFragment;
+
+import java.util.List;
+
+import static com.example.pricechecker.R.id.barcode_fragment;
+import static com.example.pricechecker.R.id.result_head;
+
+public class ScanFragment extends Fragment implements BarcodeReaderFragment.BarcodeReaderListener {
+    private static final String TAG = ScanFragment.class.getSimpleName();
+
+    private BarcodeReaderFragment barcodeReader;
+    private static TextView result_head;
+    private static TextView result;
+    private Button button;
+    private Boolean buttonClicked = false;
+
+    public static ScanFragment newInstance() {
+        Bundle args = new Bundle();
+        ScanFragment fragment = new ScanFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_scan, container, false);
+
+        barcodeReader = (BarcodeReaderFragment) getChildFragmentManager().findFragmentById(R.id.scan_fragment);
+        barcodeReader.setListener(this);
+        barcodeReader.onPause();
+//        result_head = setText(barcode.displayValue);
+        result_head = (TextView) view.findViewById(R.id.result_head);
+        result = (TextView) view.findViewById(R.id.result);
+        button = (Button) view.findViewById(R.id.btn_fragment);
+        button.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                if(buttonClicked == false){
+                    barcodeReader.onResume();
+                    buttonClicked^= true;
+                }else{
+                    barcodeReader.onPause();
+                    buttonClicked^= true;
+                }
+
+//                result.setText(buttonClicked.toString());
+            }
+        } );
+//        result_head.setText("barcode.displayValue");
+        return view;
+    }
+
+    @Override
+    public void onScanned(final Barcode barcode) {
+        Log.e(TAG, "onScanned: " + barcode.displayValue);
+        barcodeReader.playBeep();
+        result_head.setText(barcode.displayValue);
+//        result_head.setTextColor(Color.WHITE);
+        Toast.makeText(getActivity(), "Barcode: " + barcode.displayValue, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+//        Log.e(TAG, "onScannedMultiple: " + barcodes.size());
+//
+//        String codes = "";
+//        for (Barcode barcode : barcodes) {
+//            codes += barcode.displayValue + ", ";
+//        }
+//
+//        final String finalCodes = codes;
+//        result_head.setText(finalCodes);
+        Toast.makeText(getActivity(), "Please scan only one barcode at once ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+        Log.e(TAG, "onScanError: " + errorMessage);
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        Toast.makeText(getActivity(), "Camera permission denied!", Toast.LENGTH_LONG).show();
+    }
+}
